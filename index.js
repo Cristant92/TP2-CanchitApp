311b8344706f6645678ca2588e950931f6ec8c51
adriantama <tokudatamashiro@gmail.com>
2020-10-17 11:57:04 -0300

    generando array y endpoint de usuarios

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3007
  
+var usuarios = [
+    {dni: 93688330, nombre: 'Adrian', apellido: 'Tamashiro', email: 'adriantama@email.com', fecha_nacimiento: '12-03-1991'},
+    {dni: 12345678, nombre: 'Cristian', apellido: 'Ciz', email: 'crisciz@email.com', fecha_nacimiento: '16-10-1992'},
+    {dni: 12121212, nombre: 'Mariam', apellido: 'Sleiman', email: 'marsleiman@email.com', fecha_nacimiento: '04-10-1995'},
+    {dni: 99893289, nombre: 'Alejandro', apellido: 'Facchinelli', email: 'alefacchinelli@email.com', fecha_nacimiento: '21-08-1992'},
+    {dni: 43289127, nombre: 'Rocio', apellido: 'Vargas', email: 'rociovargas@email.com', fecha_nacimiento: '21-05-1992'}
+];
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -19,6 +27,10 @@ app.get('/api/user/:nombre/:apellido', (req, res) => {
     //res.send(req.params.apellido)
     //Lo envía en formato objeto en el de req.params
     res.send(req.params)
+})
+
+app.get('/api/usuarios/', (req, res) => {
+    res.send(usuarios)
 })
 
 app.listen(port, () => console.log('Escuchando puerto: ' +port))
